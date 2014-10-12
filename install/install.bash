7cc12cf66bb2f953e0ebf8ee5e38a9d5d74b5273
Chao Qu <quchao@seas.upenn.edu>
2014-10-12 16:52:27 -0400

    udpate install.sh

--- install/install.bash
+++ install/install.bash
@@ -4,6 +4,12 @@ CURRENT_DIR=$(pwd)
 REPO_NAME=ebus_sdk
 CLONE_DIR=/tmp
 
+sudo apt-get install libudev-dev
+if [[ ! -L libudev.so.0 ]]; then
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libudev.so /usr/lib/libudev.so.0
+    sudo ln -s /usr/lib/libudev.so.0 libudev.so.1
+fi
+
 cd ${CLONE_DIR}
 git clone https://github.com/versatran01/${REPO_NAME}
 cd ${REPO_NAME}
